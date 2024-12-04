2c88a95a674167127acba49e3accc04537ebdb3d
Dhyan Umeshkumar Thakkar <thakk100@umn.edu>
2024-12-04 16:57:52 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#! 5521-hw4/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn as sk
+
+def main():
+    fmnist_train = pd.read_csv('ftrain56.csv')
+    fmnist_test = pd.read_csv('ftest56.csv')
+
+if __name__ == "__main__":
+    main()
